3696ffad82d2e11367291440e05fe601159cac40
dutchkillsg <mip12793@gmail.com>
2017-12-28 15:13:54 -0500

    added mock data to test

---
+++ src/NYT.js
@@ -0,0 +1,17 @@
+const mockBooks = [
+    {
+        rank: 1,
+        title: "Gathering Prey",
+        author: "John Sandford"
+        book_image: 
+        "http:// du.ec2. nytimes.com.s3. amazonaws.com/ prd/ books/ 9780399168796.jpg"
+    },
+    {
+        rank: 2,
+        tite: "Memory Man",
+        author: "David Baldacci",
+        book_image: 
+        "http:// du.ec2. nytimes.com.s3. amazonaws.com/ prd/ books/ 9781455586387. jpg"
+    }
+
+];
